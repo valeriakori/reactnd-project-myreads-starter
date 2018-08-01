46490c93a2efaece95023098b3c1267700dc6f46
valeria <a.valeria.koriatchenko@freenet.de>
2018-08-01 13:08:54 +0200

    Search working w/o authors

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import escapeRegExp from "escape-string-regexp";
-import Book from './Book'
+import Book from "./Book";
 import * as BooksAPI from "../BooksAPI";
 
 class SearchPage extends Component {
   state = {
-    query: '',
+    query: "",
     showingBooks: []
   };
 
-  updateQuery = (query) => {
-    this.setState({query: query.trim()}, this.searchBook)
+  updateQuery = query => {
+    this.setState({ query: query.trim() }, this.searchBook);
   };
 
   searchBook = () => {
+
     if (this.state.query) {
-      BooksAPI.search(this.state.query)
-      .then(foundBooks => {
+
+      BooksAPI.search(this.state.query).then(foundBooks => {
         if (foundBooks.length) {
-          this.setState({showingBooks: foundBooks})
-          console.log(this.state.showingBooks)
-          
+          this.setState({ showingBooks: foundBooks });
+        } else {
+          this.setState({ showingBooks: [] });
         }
-      })
+      });
     } else {
-      this.setState({query: '', showingBooks: []})
+      this.setState({ query: "", showingBooks: [] });
     }
-  }
+  };
 
   render() {
+    const { query, showingBooks } = this.state;
 
-    const { query, showingBooks } = this.state
-
-      return (
+    return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">
@@ -41,20 +41,26 @@ class SearchPage extends Component {
           </Link>
           <div className="search-books-input-wrapper">
             <input
-              value={this.state.query}
-              onChange={(e) => this.updateQuery(e.target.value)}
+              value={query}
+              onChange={e => this.updateQuery(e.target.value)}
               type="text"
               placeholder="Search by title or author"
             />
           </div>
         </div>
+        
+        {/* {showingBooks.length === 0 && (
+          <p>
+            No Books found (yet). Please, update your search
+          </p>
+        )} */}
 
         {showingBooks.length > 0 && (
           <div className="search-books-results">
-          <ol className="books-grid"> 
-            {showingBooks.map((book) => <Book key={book.id} book={book}/>)}
-          </ol>
-        </div>
+            <ol className="books-grid">
+              {showingBooks.map(book => <Book key={book.id} book={book} />)}
+            </ol>
+          </div>
         )}
       </div>
     );
